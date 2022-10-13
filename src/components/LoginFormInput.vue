9626bbac74f9dc1b551848ee0b67a8a7b6e5307c
M4rceleq <maciaszczyk.marcel@gmail.com>
2022-10-13 22:14:16 +0200

    Add LoginFormInput component

---
+++ src/components/LoginFormInput.vue
@@ -0,0 +1,24 @@
+<template>
+    <label class="text-white" for="email">{{ labelContent }}</label>
+    <input 
+        class="text-white bg-gray-dark py-2 px-4 w-full border-2 border-solid border-white focus:border-green rounded"
+        :value="modelValue" @input="updateValue" :id="name" :name="name" :type="type" :autocomplete="autocomplete" required
+    >
+</template>
+
+<script setup lang="ts">
+interface Props {
+  labelContent: string;
+  type: string;
+  name: string;
+  autocomplete?: string;
+  modelValue: string;
+}
+
+const emit = defineEmits(['update:modelValue']);
+const updateValue = (e: Event) => {
+  emit('update:modelValue', (e.target as HTMLInputElement).value)
+};
+
+const props = defineProps<Props>();
+</script>
