9efc8ab3ff9515b679d592da262482228efd7c90
Mikutut <marcinmikula840@gmail.com>
2022-10-14 19:52:48 +0200

    Untrack `.env` file (...)
    Other changes:
    - Add `POSITIONSTACK_API_KEY` to `ImportMetaEnv`
    - Update `.gitignore`

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-    readonly BACKEND_URL: string
+    readonly BACKEND_URL: string;
+    readonly POSITIONSTACK_API_KEY: string;
     // more env variables...
 }
 
