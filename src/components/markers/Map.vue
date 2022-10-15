48662ea522bc10a3245c72166864d448df7b1177
M4rceleq <maciaszczyk.marcel@gmail.com>
2022-10-15 22:31:42 +0200

    dark theme map

--- src/components/markers/Map.vue
+++ src/components/markers/Map.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="map-box w-full h-full flex flex-col justify-center items-center text-center text-white text-6xl">
-    <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" class="w-full h-full" >
+    <ol-map ref="map" :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" class="w-full h-full" >
       <ol-view 
         ref="view" 
         :center="center" 
-        :rotation="rotation" 
         :zoom="zoom" 
-        :projection="projection" 
       />
+      <!-- <ol-zoom-control />  -->
       <ol-tile-layer>
-          <ol-source-osm />
+        <ol-source-xyz crossOrigin='anonymous' url="https://c.tile.jawg.io/jawg-dark/{z}/{x}/{y}.png?access-token=87PWIbRaZAGNmYDjlYsLkeTVJpQeCfl2Y61mcHopxXqSdxXExoTLEv7dwqBwSWuJ" />
       </ol-tile-layer>
       <ul>
         <li v-for="todo in todos" :key="todo.id">
@@ -26,13 +25,14 @@
   import type { Pointer as IPointer, PointerProps } from '@/types';
   import type { Ref } from 'vue';
 
-  const center = ref([ 19.1198,50.278502]);
-  const projection = ref('EPSG:4326');
-  const zoom = ref(19);
-  const rotation = ref(0);
+  const map = ref();
+  console.log(map);
+  const center = ref([19.191665, 51.8803198]);
+  const zoom = ref(6.5);
   const l1 = ref('')
   const l2 = ref('')
   let id = 0;
+
   let dix: PointerProps = 
   {
     caption: "",
