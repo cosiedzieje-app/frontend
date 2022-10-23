6b944de66f4eb3fdb79de6a4d20d617e32c25f6d
Mikutut <marcinmikula840@gmail.com>
2022-10-23 19:32:40 +0200

    yes

--- src/types/index.ts
+++ src/types/index.ts
@@ -27,6 +27,8 @@ interface Store {
   currentlyExploredMarker: Marker | null;
 }
 
+type CustomButtonType = "button" | "submit" | "reset";
+
 // CustomButton props format
 interface ButtonProps {
   caption: string;
@@ -35,7 +37,7 @@ interface ButtonProps {
   icon: string;
   enabled?: boolean;
   mobile?: boolean;
-  type?: "button" | "submit" | "reset";
+  type?: CustomButtonType;
 }
 
 // Form radio format
@@ -242,6 +244,7 @@ interface FilteredMarkersData {
 
 export type {
   ButtonProps,
+  CustomButtonType,
   FormRadioProps,
   AccountSidebarItemProps,
   Store,
